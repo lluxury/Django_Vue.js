7dd370b74bc304f8c0d6fa8da69298798ba6c843
tmp4321 <tmp4321@qq.com>
2019-05-11 14:01:13 +0800

    django using vue
    Signed-off-by: tmp4321 <tmp4321@qq.com>

--- project01/yann_manager/settings.py
+++ project01/yann_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jq@1jo_z=rs4r5weke7!&i@p5nr)4!di#=dl337gd8o(bv-ih4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['47.103.79.243']
 
 
 # Application definition
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'yann_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [],
+        'DIRS': ['frontend/dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/dist/static"),
+]
